3564907e5e181c9a756e4ccd1a4d0ad9a10e1c4c
toddn <tcnichol@illinois.edu>
2022-05-13 14:50:45 -0500

    lessons 1 and 2 added

--- lessons/2_read_write_text_files.py
+++ lessons/2_read_write_text_files.py
@@ -1,6 +1,32 @@
 import os
+import datetime
 
-def main():
+def deleting_files():
+    files_in_current_dir = os.listdir(os.getcwd())
+    print('all the files in the current dir')
+    print(files_in_current_dir)
+    for f in files_in_current_dir:
+        if f.endswith('.txt'):
+            try:
+                os.remove(os.path.join(os.getcwd(), f))
+            except Exception as e:
+                print('could not remove file')
+                print(e)
+
+def writing_file():
+    right_now = datetime.datetime.now()
+    print('right now it is', str(right_now))
+    print('we will put this in a new text file')
+    # this method makes a readable string that youc an format
+    readable_now = right_now.strftime("%m_%d_%Y_%H:%M:%S")
+    new_text_file_name = 'text_file_' + readable_now + '.txt'
+    with open(new_text_file_name, 'w') as f:
+        # note the inclusion of the new line character
+        f.write('We are writing the current date and time\n')
+        f.write(readable_now+'\n')
+    print('done here')
+
+def reading_file():
     current_location = os.getcwd()
 
     path_components = current_location.split('/')
@@ -26,4 +52,7 @@ def main():
     print('done')
 
 if __name__ == "__main__":
-    main()
+    writing_file()
+    reading_file()
+    # this next one deletes all the text files that the writing files part created
+    deleting_files()
